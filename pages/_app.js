eab067f1132c6111aacbc19f352233238d0f2ce7
sasadra <sasaworkpro@gmail.com>
2022-09-21 11:43:59 -0700

    Created Auth layer and added login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const Layout = Component.layout || (({ children }) => <>{children}</>);
+
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...colors,
+    },
     extend: {},
   },
   plugins: [],
