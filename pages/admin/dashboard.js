4436d1833663905cad0b9aba26bde4e6c87f59c6
sasadra <sasaworkpro@gmail.com>
2022-10-04 00:57:12 -0700

    Init Admin Dashboard page

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// layout for page
+
+import Admin from "layouts/Admin.js";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="flex flex-wrap">
+        Dashboard Coming soon.
+      </div>
+    </>
+  );
+}
+
+Dashboard.layout = Admin;
